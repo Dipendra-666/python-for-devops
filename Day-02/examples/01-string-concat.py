bbc8115a7ce8cbd45d9169a9c40e6681952fb250
Dipendra <rayamajhidipendra702@gmail.com>
2026-04-26 19:42:30 +0545

    learned about concat in python

--- Day-02/examples/01-string-concat.py
+++ Day-02/examples/01-string-concat.py
@@ -1,4 +1,9 @@
-str1 = "Hello"
-str2 = "World"
+# str1 = "Hello"
+# str2 = "World"
+# result = str1 + " " + str2
+# print(result)
+
+str1 = "Dipendra"
+str2 = "Rayamajhi"
 result = str1 + " " + str2
 print(result)
